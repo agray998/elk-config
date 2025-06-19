9d3d49556fab951f153a8df07f22693e4c169566
agray <agray@qa.com>
2025-06-19 11:13:00 +0100

    ensure helm repo

--- install-elk.sh
+++ install-elk.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+helm repo add elastic https://helm.elastic.co
+helm repo update
 kubectl create ns logging
 helm -n logging install elk-elasticsearch elastic/elasticsearch -f values-e.yaml
 helm -n logging install elk-kibana elastic/kibana -f values-k.yaml 
